a16c5bdf8c65c7effc916d96dc505c101a343808
JurassicSWE <38356547+JurassicSWE@users.noreply.github.com>
2018-09-07 08:11:00 +0200

    Update xmlGenerator.js

--- serverSide/Tier/ApplicationTier/generators/xmlGenerator.js
+++ serverSide/Tier/ApplicationTier/generators/xmlGenerator.js
@@ -2,7 +2,7 @@
 * File: xmlGenerator.js
 * Version: 1.0
 * Type: javascript
-* Date: 2018-08-16
+* Date: 2018-09-06
 * Author: Leo Moz
 * E-mail: JurassicSWE@gmail.com
 *
@@ -21,33 +21,38 @@ module.exports = class XMLGenerator {
     generateConfig(data) {
 
         this.code += ('<?xml version = "1.0" encoding = "UTF-8"?>\n');
-        //this.code += ('<!DOCTYPE hibernate-configuration SYSTEM "http://www.hibernate.org/dtd/hibernate-configuration-3.0.dtd">\n\n');
-        this.code += ('<!DOCTYPE hibernate-configuration PUBLIC "-//Hibernate/Hibernate Configuration DTD 5.3//EN" "http://hibernate.sourceforge.net/hibernate-configuration-5.3.dtd"> \n \n');
+         //this.code += ('<!DOCTYPE hibernate-configuration PUBLIC "-//Hibernate/Hibernate Configuration DTD 3.0//EN" "http://www.hibernate.org/dtd/hibernate-configuration-3.0.dtd">\n\n');
+        this.code += ('<!DOCTYPE hibernate-configuration PUBLIC "-//Hibernate/Hibernate Configuration DTD 5.3//EN" "http://hibernate.sourceforge.net/hibernate-configuration-5.3.dtd">\n\n');
         this.code += ('<hibernate-configuration>\n');
         this.code += ('   <session-factory>\n\n');
 
-		this.code += ('      <property name="hbm2ddl.auto">\n');
-		this.code += ('          update');
-		this.code += ('      </property>\n\n');
-        this.code += ('      <property name="dialect">\n');
-        this.code += ('          org.hibernate.dialect.MySQLDialect\n');
-        this.code += ('      </property>\n\n');
+		//this.code += ('      <property name="hibernate.hbm2ddl.auto">\n');
+		//this.code += ('          update');
+		//this.code += ('\n      </property>\n\n');
+		
+        this.code += ('      <property name="hibernate.dialect">\n');
+        this.code += ('          org.hibernate.dialect.MySQLDialect');
+        this.code += ('\n      </property>\n\n');
 
-        this.code += ('      <property name="connection.driver_class">\n');
-        this.code += ('          com.mysql.jdbc.Driver\n');
-        this.code += ('      </property>\n\n');
+        this.code += ('      <property name="hibernate.connection.driver_class">\n');
+        this.code += ('          com.mysql.cj.jdbc.Driver'); //com.mysql.jdbc.Driver
+        this.code += ('\n      </property>\n\n');
 
-        this.code += ('      <property name="connection.url">\n');
-        this.code += ('          jdbc:mysql://localhost:80/ironworks\n');
-        this.code += ('      </property>\n\n');
+        this.code += ('      <property name="hibernate.connection.url">\n');
+        this.code += ('          jdbc:mysql://url-db');
+							/* esempio jdbc:mysql://localhost:3306/ironworks_db
+							 * in caso di lamentele da parte di hibernate con l'orario del server, aggiungere in coda all'url:
+							 * ?autoReconnect=true&amp;useSSL=false&amp;useLegacyDatetimeCode=false&amp;serverTimezone=UTC
+							 */
+        this.code += ('\n      </property>\n\n');
 
-        this.code += ('      <property name="connection.username">\n');
-        this.code += ('          root\n');
-        this.code += ('      </property>\n\n');
+        this.code += ('      <property name="hibernate.connection.username">\n');
+        this.code += ('          db-user');
+        this.code += ('\n      </property>\n\n');
 
-        this.code += ('      <property name="connection.password">\n');
-        this.code += ('          ironworks\n');
-        this.code += ('      </property>\n\n');
+        this.code += ('      <property name="hibernate.connection.password">\n');
+        this.code += ('          db-password');
+        this.code += ('\n      </property>\n\n');
 
         this.code += ('      <mapping resource="diagram.hbm.xml"/>\n\n')
 
@@ -66,34 +71,38 @@ module.exports = class XMLGenerator {
         this.code += ('<?xml version = "1.0" encoding = "UTF-8"?>\n');
         this.code += ('<!DOCTYPE hibernate-mapping PUBLIC "-//Hibernate/Hibernate Mapping DTD 5.3//EN" "http://hibernate.sourceforge.net/hibernate-mapping-5.3.dtd">\n\n');
 
-        this.code += ('<hibernate-mapping>\n \n');
+        this.code += ('<hibernate-mapping>\n\n');
           for (let i = 0; i < data.length; i++) {
               let entity = data[i];
-              this.code += ('<class name="com.jurassicswe.ironworks.diagram.');
+              this.code += ('<class name="ironworks.');
 			  this.code += entity.name;
 			  this.code += ('" table="');
               this.code += entity.name;
               this.code += ('">\n');
-              let Prim=false;
+              let pk=false;
               let attrs=entity.attr;
               if(attrs.length > 0) {
+				let codeaux='';
                 for(let i = 0; i < attrs.length; i++) {
                   let attribute=attrs[i];
                   if(attribute.primaryKey=='true'){
-                    Prim=true;
+                    pk=true;
                     this.code += ('\t<id name="');
                     this.code += attribute.name;
                     this.code += ('"></id>\n');
                   }else{
-                    this.code += ('\t<property name="');
-                    this.code += attribute.name;
-                    this.code += ('"></property>\n');
+                    codeaux += ('\t<property name="');
+                    codeaux += attribute.name;
+                    codeaux += ('"></property>\n');
                   }
-                }
-                if(!Prim){
+                }				
+                if(!pk)
                   this.code += ('\t<id name="id">\n\t\t<generator class="assigned"></generator>\n\t</id>\n');
-                }
-              }
+			    this.code += codeaux;
+				codeaux = '';
+              }else{
+					this.code += ('\t<id name="id">\n\t\t<generator class="assigned"></generator>\n\t</id>\n');
+			  }
               this.code += ('</class>\n\n');
           }
           this.code += ('</hibernate-mapping>');
